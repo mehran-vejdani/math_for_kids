858d9efbc37bc15a35f81fd4688130e5f80aa481
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-06 22:07:11 +0430

    add function to options

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ const WrongAudio = document.querySelector(".myAudio");
 const option1 = document.querySelector(".option1");
 const option2 = document.querySelector(".option2");
 const option3 = document.querySelector(".option3");
+
+let answer = 0;
+
+function generate_equation() {}
+
+option1.addEventListener("click", () => {
+  if (option1.innerHTML == answer) {
+    generate_equation();
+  } else {
+    WrongAudio.play();
+  }
+});
+option2.addEventListener("click", () => {
+  if (option2.innerHTML == answer) {
+    generate_equation();
+  } else {
+    WrongAudio.play();
+  }
+});
+option3.addEventListener("click", () => {
+  if (option3.innerHTML == answer) {
+    generate_equation();
+  } else {
+    WrongAudio.play();
+  }
+});
