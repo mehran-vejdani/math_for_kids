2ff49979059c0c54aa04d004ac8c4912bfa22f7c
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-06 22:18:25 +0430

    add selector number1 & number2 and make a random number   in to generate_equation function

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const option1 = document.querySelector(".option1");
 const option2 = document.querySelector(".option2");
 const option3 = document.querySelector(".option3");
 
+const number1 = document.querySelector(".num1");
+const number2 = document.querySelector(".num2");
+
 let answer = 0;
 
-function generate_equation() {}
+function generate_equation() {
+  let num1 = Math.floor(Math.random() * 13);
+  let num2 = Math.floor(Math.random() * 13);
+  let dummyAnswer1 = Math.floor(Math.random() * 13);
+  let dummyAnswer2 = Math.floor(Math.random() * 13);
+
+  let addAnswer = [];
+  answer = num1 + num2;
+  number1.innerHTML = num1;
+  number2.innerHTML = num2;
+
+  option1.innerHTML = answer;
+  option2.innerHTML = dummyAnswer1;
+  option3.innerHTML = dummyAnswer2;
+}
 
 option1.addEventListener("click", () => {
   if (option1.innerHTML == answer) {
