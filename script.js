761e24cac70d24d1f671ccedfe3264b5f36d0e24
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-06 22:03:57 +0430

    get selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const WrongAudio = document.querySelector(".myAudio");
+const option1 = document.querySelector(".option1");
+const option2 = document.querySelector(".option2");
+const option3 = document.querySelector(".option3");
